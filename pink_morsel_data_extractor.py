5692643e76d1100c7a351a46e60601b816193090
Jason Loi <z5420621@ad.unsw.edu.au>
2025-03-30 18:56:24 +1100

    Pink morsel data processing to csv

---
+++ pink_morsel_data_extractor.py
@@ -0,0 +1,44 @@
+import csv
+import pandas as pd
+import glob
+
+PRODUCT_FILTER = "pink morsel"
+
+def concat_data():
+    path = "data/*.csv"
+    data_files = glob.glob(path)
+
+    li = []
+    for data_file in data_files:
+        df = pd.read_csv(data_file, index_col=None, header=0)
+        li.append(df)
+
+    frame = pd.concat(li, axis=0, ignore_index=True)
+
+    return frame
+
+
+def filter_pink_morsel(frame):
+    return frame[frame["product"] == PRODUCT_FILTER]
+
+
+def calculate_sale(quantity: int, price: str):
+    price = float(price[1:])
+    sale = quantity * price
+
+    return "{:.2f}".format(sale)
+
+def extract_data(frame):
+    with open("processed_data.csv", mode="w") as processed_file:
+        processed_writer = csv.writer(
+            processed_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        for _, row in frame.iterrows():
+            sale = calculate_sale(row["quantity"], row["price"])
+            processed_writer.writerow([f"${sale}", row["date"], row["region"]])
+
+if __name__ == "__main__":
+    frame = concat_data()
+    pink_morsel_only_frame = filter_pink_morsel(frame)
+
+    extract_data(pink_morsel_only_frame)
